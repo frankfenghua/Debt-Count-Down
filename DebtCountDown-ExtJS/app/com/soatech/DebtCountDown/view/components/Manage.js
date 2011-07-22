20c529956cc3fb699900561151b6fec16f495888
Eric Jensen <ejensen@pathf.com>
2011-07-21 21:03:43 -0500

    Manage view changes

--- DebtCountDown-ExtJS/app/com/soatech/DebtCountDown/view/components/Manage.js
+++ DebtCountDown-ExtJS/app/com/soatech/DebtCountDown/view/components/Manage.js
@@ -5,11 +5,101 @@ DebtCountDown.view.components.Manage = Ext.extend(DebtCountDown.view.components.
 	},
 	
 	initializeChildren: function() {
-		Ext.create('Ext.panel.Panel', {
+		Ext.create('Ext.data.Store', {
+		    storeId:'planStore',
+		    fields:['name'],
+		    data:{'items':[
+		        {"name":"Plan One"},
+		    ]},
+		    proxy: {
+		        type: 'memory',
+		        reader: {
+		            type: 'json',
+		            root: 'items'
+		        }
+		    }
+		});
+		
+		Ext.create('Ext.data.Store', {
+		    storeId:'debtStore',
+		    fields:['name'],
+		    data:{'items':[
+		        {"name":"Debt One"},
+		    ]},
+		    proxy: {
+		        type: 'memory',
+		        reader: {
+		            type: 'json',
+		            root: 'items'
+		        }
+		    }
+		});
+		
+		Ext.create('Ext.grid.Panel', {
+			id: 'managePlansGrid',
+		    title: 'Manage Plans',
+		    store: Ext.data.StoreManager.lookup('planStore'),
+		    columns: [
+		        {header: 'Name',  dataIndex: 'name'}
+		    ],
+		    height: 500,
+		    width: 400,
+		    renderTo: Ext.getBody(),
+		    hideHeaders:true
+		});
+		
+		Ext.create('Ext.grid.Panel', {
+			id: 'manageDebtsGrid',
+		    title: 'Manage Grids',
+		    store: Ext.data.StoreManager.lookup('debtStore'),
+		    columns: [
+		        {header: 'Name',  dataIndex: 'name'}
+		    ],
+		    height: 500,
+		    width: 400,
+		    renderTo: Ext.getBody(),
+		    hideHeaders:true,
+		    hidden: true,
+		    hideMode: 'Offsets'
+		});
+		
+		Ext.create('Ext.Button',{
+			id: 'managePlansBtn',
+			text: 'Manage Plans',
 			renderTo: Ext.getBody(),
-			width: 400,
-			height: 500,
-			title: 'Manage Plans'
+			scale: 'large',
+			enableToggle: true,
+			height: 50,
+			width: 200,
+			pressed: true,
+			toggleGroup: "mainNav",
+			toggleHandler: function() 
+			{
+				if(this.pressed)
+				{
+					Ext.get('managePlansGrid').toggle();
+					Ext.get('manageDebtsGrid').toggle();
+				}
+			}
+		});
+		
+		Ext.create('Ext.Button',{
+			id: 'manageDebtsBtn',
+			text: 'Manage Debts',
+			renderTo: Ext.getBody(),
+			scale: 'large',
+			enableToggle: true,
+			height: 50,
+			width: 200,
+			toggleGroup: "mainNav",
+			toggleHandler: function() 
+			{
+				if(this.pressed)
+				{
+					Ext.get('managePlansGrid').toggle();
+					Ext.get('manageDebtsGrid').toggle();
+				}
+			}
 		});
 	},
 	
