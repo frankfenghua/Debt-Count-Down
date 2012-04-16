a95950fb93cc821735e2f3721f16f2bf96164725
Eric Jensen <ejensen@follettsoftware.com>
2012-04-16 10:11:40 -0500

    Created DynamoDB server

---
+++ DebtCountDown-Server-DynamoDB/php/dcd_server.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'sdk/sdk.class.php';
+require_once 'services/PlanService.php';
+
+// set the various include paths so we can have an easier time auto-loading
+$appended_inc_path = './services';
+set_include_path(get_include_path() . PATH_SEPARATOR . $appended_inc_path);
+
+
+// Auto-load classes so you don't have to worry about managing requires
+function __autoload($class) {
+	echo $class;
+	include_once $class . '.php';
+}
+
+CFCredentials::set(array(
+	'production' => array(
+		'key' => 'AKIAJVKEBQPVUIQI5BDQ',
+		'secret' => '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',
+		'cache' => 'apc'
+	)
+));
+
+// Set a default action
+$request_service = $_REQUEST['service'] . 'Service';
+$request_action = $_REQUEST['action'];
+
+$service = new $request_service;
+
+if(is_callable(array($service,$request_action))) {
+	call_user_func(array($service,$request_action), $_REQUEST);
+}
+
+?>
---
+++ DebtCountDown-Server-DynamoDB/php/services/PlanService.php
@@ -0,0 +1,197 @@
+<?
+class PlanService
+{
+	//-------------------------------------------------------------------------
+	//
+	// Variables
+	//
+	//-------------------------------------------------------------------------
+
+	/**
+	 * @private
+	 */
+	private $db;
+
+	/**
+	 * @private
+	 */
+	private $queue;
+
+	//-------------------------------------------------------------------------
+	//
+	// Constructor
+	//
+	//-------------------------------------------------------------------------
+
+	public function __construct()
+	{
+		try
+		{
+			$this->db = new AmazonDynamoDB();
+
+			// $this->db->credentials = array()
+			
+		}
+		catch( PDOException $e )
+		{
+			print_r($e);
+		}
+	}
+
+	//-------------------------------------------------------------------------
+	//
+	// Methods
+	//
+	//-------------------------------------------------------------------------
+
+	/**
+	 * @param array $params
+	 */
+	public function addPlan( $params )
+	{
+		$retval = '';
+
+		try
+		{
+			$sth = $this->db->prepare("INSERT INTO plans (name) VALUES (?)");
+			$sth->execute(array($params['name']));
+
+			$retval = '{"pid":"' . $this->db->lastInsertId() . '"}';
+		}
+		catch( PDOException $e )
+		{
+			print_r($e);
+		}
+
+		echo $retval;
+	}
+
+	/**
+	 * @param array $params
+	 */
+	public function deletePlan( $params )
+	{
+		$sth = $this->db->prepare("DELETE FROM plans WHERE pid = ?");
+		$sth->execute(array($params['pid']));
+	}
+
+	/**
+	 * @param array $params
+	 */
+	public function importData( $params )
+	{
+		echo PHP_EOL . PHP_EOL;
+		echo "# Adding data to the table..." . PHP_EOL;
+
+		$queue = new CFBatchRequest();
+		$queue->user_credentials($db->credentials);
+
+		$db->batch($queue)->put_item(array(
+			'TableName' => 'DCD-Plans',
+			'Item' => array(
+				'pid' => array( AmazonDynamoDB::TYPE_NUMBER => '101' ),
+				'name' => 'Plan One'
+			)
+		));
+
+		$db->batch($queue)->put_item(array(
+			'TableName' => 'DCD-Plans',
+			'Item' => array(
+				'pid' => array( AmazonDynamoDB::TYPE_NUMBER => '102' ),
+				'name' => 'Plan Two'
+			)
+		));
+
+		$db->batch($queue)->put_item(array(
+			'TableName' => 'DCD-Plans',
+			'Item' => array(
+				'pid' => array( AmazonDynamoDB::TYPE_NUMBER => '103' ),
+				'name' => 'Plan Three'
+			)
+		));
+	}
+
+	/**
+	 * @param $params
+	 * @return string
+	 */
+	public function loadFullPlan( $params )
+	{
+		$retval = "";
+
+		try 
+		{
+			$sth = $this->db->prepare("SELECT d.pid, name, bank, balance, apr, paymentRate FROM debts d INNER JOIN planDebts pd ON pd.debtId = d.pid AND pd.planId = ?");
+			$sth->execute(array($params['planId']));
+
+			$debts = $sth->fetchAll();
+
+			$sth = $this->db->prepare("SELECT SUM(i.amount) as income, " .
+				"SUM(e.amount) AS expenses " .
+				"FROM planBudgetItems pbi " .
+				"LEFT JOIN budgetItems e ON e.pid = pbi.budgetItemId AND e.type = 'EXPENSE' " .
+				"LEFT JOIN budgetItems i ON i.pid = pbi.budgetItemId AND i.type = 'INCOME' " .
+				"WHERE pbi.planId = ?");
+
+			$sth->execute(array($params['planId']));
+
+			$budget = $sth->fetch();
+
+			$retset = array('debts' => $debts, 'budget' => $budget);
+
+			$retval = json_encode($retset);
+		} 
+		catch (PDOException $e) 
+		{
+			error_log(print_r($e,1));
+		}
+
+		echo $retval;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function loadAllPlans()
+	{
+		$retval = "";
+
+		try
+		{
+			$sth = $this->db->prepare("SELECT pid, name FROM plans");
+			$sth->execute();
+
+			$plans = $sth->fetchAll();
+
+			$retval = json_encode($plans);
+		}
+		catch(PDOException $e)
+		{
+			print_r($e);
+		}
+
+		echo $retval;
+	}
+
+	/**
+	 * @param array $params
+	 */
+	public function loadPlan( $params )
+	{
+		$sth = $this->db->prepare("SELECT pid, name FROM plans WHERE pid = ?");
+		$sth->execute(array($params['pid']));
+		$plan = $sth->fetch();
+
+		echo json_encode($plan);
+	}
+
+	/**
+	 * @param array $params
+	 */
+	public function updatePlan( $params )
+	{
+		$sth = $this->db->prepare("UPDATE plans SET name = ? WHERE pid = ?");
+		$sth->execute(array($params['plan']['name'], $params['plan']['pid']));
+	}
+}
+?>
